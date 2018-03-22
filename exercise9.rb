551cd4a59f882304cc90251eb9d9da7a840ec3b8
gusmattos1 <gusmattos@gmail.com>
2018-03-21 20:53:45 -0400

    exercise 10.4 done

--- exercise9.rb
+++ exercise9.rb
@@ -3,10 +3,31 @@ students = {
   :cohort2 => 42,
   :cohort3 => 22
 }
+puts "display students\n\n"
 
+#method to display the list
 def display (x)
   x.each do |x|
-    puts"#{x} students"
+    puts"#{x} students\n\n"
   end
 end
+
+
 display (students)
+
+
+puts "include the cohort and studentes on the list"
+
+#method to include on the list
+def inclu (list, cohort, students)
+  list[cohort] = students
+  display(list)
+end
+
+inclu(students, :cohort4, 43)
+
+def cohortexport (list)
+puts list.keys
+end
+
+cohortexport (students)
